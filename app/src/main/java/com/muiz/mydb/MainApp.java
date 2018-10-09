3ab7b8f9b45d5ed4c025d94a4acf770883731eac
razamurad100 <38163131+razamurad100@users.noreply.github.com>
2018-10-09 17:32:11 +0500

    Added Signout option

--- app/src/main/java/com/muiz/mydb/MainApp.java
+++ app/src/main/java/com/muiz/mydb/MainApp.java
@@ -1,25 +1,25 @@
 package com.muiz.mydb;
 
-import android.support.design.widget.TabLayout;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
 public class MainApp extends AppCompatActivity {
+
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -40,6 +40,7 @@ public class MainApp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main_app );
+
 
         Toolbar toolbar = (Toolbar) findViewById( R.id.toolbar );
         setSupportActionBar( toolbar );
@@ -84,11 +85,21 @@ public class MainApp extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+                Intent intent = new Intent( MainApp.this,LoginActivity.class );
+            intent.addFlags( Intent.FLAG_ACTIVITY_CLEAR_TASK );
+            intent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
+            startActivity( intent );
             return true;
         }
 
         return super.onOptionsItemSelected( item );
     }
+
+
+
+
+
+
 
     /**
      * A placeholder fragment containing a simple view.
